2269da3b5527e92347fd1d6c56daa6f668173a01
GustavoFantin <gustavofantinbarros@gmail.com>
2025-03-05 12:40:19 -0800

    exercise done

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,51 @@
 // Check the README.md file for instructions to the exercise
+import http from 'http'
+import fs from 'fs'
+import path from 'path'
+import url from 'url'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+const directory = "images"
+const filePath = path.join(__dirname, directory)
+
+const server = http.createServer((req, res) => {
+
+    
+    const { method } = req
+    const parsedUrl = url.parse(req.url || '', true)
+    const { pathname } = parsedUrl
+    console.log(filePath)
+
+    if (pathname === '/' && method === 'GET') {
+        res.writeHead(200, { "content-type": 'text/plain' })
+        res.end("Hello from my server!")
+        return
+    }   
+    
+
+
+    if (pathname === '/view-image' && method === 'GET') {
+        fs.readFile(`${filePath}/veryhappydog.jpg`, (err, data) => {
+            if (err) {
+                res.writeHead(500, { "content-type": "image/jpeg" })
+                res.end("Eror reading file!")
+                return
+            }
+
+            res.writeHead(200, { "content-type": "image/jpeg" })
+            res.end(data)
+        })
+        return
+    }   
+    
+
+
+
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
